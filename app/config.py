eecaab3f0d2df54811bafe68167a03ce8cfbf25a
Malik Edwards <malikedwards777@gmail.com>
2021-05-08 20:59:12 -0500

    payment handling intial

--- app/config.py
+++ app/config.py
@@ -10,6 +10,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://root:''@localhost/food_delivery'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = './uploads'
+    STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+    STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+    STRIPE_ENDPOINT_SECRET = os.environ.get('STRIPE_ENDPOINT_SECRET')
 
 
 class DevelopmentConfig(Config):
--- app/routes/OrderRoute.py
+++ app/routes/OrderRoute.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
-from flask import redirect, url_for, session, request
+from flask import redirect, url_for, session, request, abort
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from ..system_management.OrderManager import OrderManager
 from ..database.db_access import order_access
 from ..database.db_access import order_groceries_access
 from ..database.db_access import payment_access
+import stripe
 
 manage_order = Blueprint("manage_order", __name__)
 
@@ -105,6 +106,26 @@ def get_total():
     else:
         return {'msg':'you are not logged in as an employee', 'error':'auth-0001'}, 401
 
+@manage_order.route('/get_schedule', methods=['POST','GET'])
+@jwt_required()
+def get_schedule():
+    user = get_jwt_identity()
+    if user and ('role' in user):
+        try:
+            orders = order_manager.getSchedule()
+            if orders:
+                response = {'msg':'success', 'data':{'orders':orders}}, 200
+            else:
+                response = {'msg':'no orders found', 'data':{'orders':orders}}, 200
+        except Exception as e:
+            print(e)
+            response = {'msg':'', 'error':'ise-0001'}, 500
+        finally:
+            return response
+    else:
+        return {'msg':'you are not logged in as an employee', 'error':'auth-0001'}, 401
+
+
 @manage_order.route('/record_payment', methods=['POST','GET'])
 @jwt_required()
 def record_payment():
@@ -124,21 +145,55 @@ def record_payment():
     else:
         return {'msg':'you are not logged in as an employee', 'error':'auth-0001'}, 401
 
-@manage_order.route('/get_schedule', methods=['POST','GET'])
+@manage_order.route('/pay')
 @jwt_required()
-def get_schedule():
-    user = get_jwt_identity()
-    if user and ('role' in user):
-        try:
-            orders = order_manager.getSchedule()
-            if orders:
-                response = {'msg':'success', 'data':{'orders':orders}}, 200
-            else:
-                response = {'msg':'no orders found', 'data':{'orders':orders}}, 200
-        except Exception as e:
-            print(e)
-            response = {'msg':'', 'error':'ise-0001'}, 500
-        finally:
-            return response
-    else:
-        return {'msg':'you are not logged in as an employee', 'error':'auth-0001'}, 401
+def pay():
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': 'YOUR_PRODUCT_PRICE_ID',
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('index', _external=True),
+    )
+    return {
+        'checkout_session_id': session['id'], 
+        'checkout_public_key': app.config['STRIPE_PUBLIC_KEY']
+    }
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print('WEBHOOK CALLED')
+
+    if request.content_length > 1024 * 1024:
+        print('REQUEST TOO BIG')
+        abort(400)
+    payload = request.get_data()
+    sig_header = request.environ.get('HTTP_STRIPE_SIGNATURE')
+    endpoint_secret = 'YOUR_ENDPOINT_SECRET'
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        print('INVALID PAYLOAD')
+        return {}, 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print('INVALID SIGNATURE')
+        return {}, 400
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        print(session)
+        line_items = stripe.checkout.Session.list_line_items(session['id'], limit=1)
+        print(line_items['data'][0]['description'])
+
+    return {}
