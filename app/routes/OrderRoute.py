460884942d3bc741b1cca049ee330e45c1a06318
Malik Edwards <malikedwards777@gmail.com>
2021-05-09 19:46:55 -0500

    update

--- app/routes/OrderRoute.py
+++ app/routes/OrderRoute.py
@@ -145,23 +145,46 @@ def record_payment():
     else:
         return {'msg':'you are not logged in as an employee', 'error':'auth-0001'}, 401
 
-@manage_order.route('/pay')
-@jwt_required()
+# AJAX endpoint when `/pay` is called from client
+@manage_order.route('/pay', methods=['POST'])
 def pay():
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[{
-            'price': 'YOUR_PRODUCT_PRICE_ID',
-            'quantity': 1,
-        }],
-        mode='payment',
-        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url=url_for('index', _external=True),
-    )
-    return {
-        'checkout_session_id': session['id'], 
-        'checkout_public_key': app.config['STRIPE_PUBLIC_KEY']
-    }
+  data = request.get_json()
+  intent = None
+
+  try:
+    if 'payment_method_id' in data:
+      # Create the PaymentIntent
+      intent = stripe.PaymentIntent.create(
+        payment_method = data['payment_method_id'],
+        amount = 1099,
+        currency = 'usd',
+        confirmation_method = 'manual',
+        confirm = True,
+      )
+    elif 'payment_intent_id' in data:
+      intent = stripe.PaymentIntent.confirm(data['payment_intent_id'])
+  except stripe.error.CardError as e:
+    # Display error on client
+    return json.dumps({'error': e.user_message}), 200
+
+  return generate_response(intent)
+
+def generate_response(intent):
+  # Note that if your API version is before 2019-02-11, 'requires_action'
+  # appears as 'requires_source_action'.
+  if intent.status == 'requires_action' and intent.next_action.type == 'use_stripe_sdk':
+    # Tell the client to handle the action
+    return json.dumps({
+      'requires_action': True,
+      'payment_intent_client_secret': intent.client_secret,
+    }), 200
+  elif intent.status == 'succeeded':
+    # The payment didn’t need any additional actions and completed!
+    # Handle post-payment fulfillment
+    return json.dumps({'success': True}), 200
+  else:
+    # Invalid status
+    return json.dumps({'error': 'Invalid PaymentIntent status'}), 500
 
 
 @app.route('/webhook', methods=['POST'])
